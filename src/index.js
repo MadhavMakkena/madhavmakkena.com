93fc48ebe53c065fd98405c1fb9d315c536c1034
Madhav Makkena <madhav.makkena@gmail.com>
2021-04-25 20:47:33 -0300

    Added more images, get image list dynamically, made order random

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
+// TODO: random pet order
+// TODO: resize images, only download when they appear
+// TODO: 3x2 randomized side-by-side image squares
+// TODO: responsive design
+// TODO: react
+// TODO: SEO
+
 let active = 0;
+let numPets = 0;
 
-const pets = [
-  'Charlotte',
-  'Leo',
-  'Lucky',
-  'Peeper'
-];
-
-function getRandomTime(min, max) {
-  return (Math.random() * (max - min) + min) * 1000;
+function getRandom(min, max) {
+  return Math.floor((Math.random() * (max - min) + min));
 }
 
 (function() {
@@ -16,18 +17,31 @@ function getRandomTime(min, max) {
     const oldActive = document.getElementsByClassName('active')[0];
     oldActive.classList.remove('active');
     setTimeout(() => {
-      const elem = document.getElementById(pets[number]);
+      const elem = document.getElementById(`pet-${number}`);
       elem.classList.add('active');
-    }, getRandomTime(0.5, 1));
+    }, getRandom(0.5, 1) * 1000);
   }
 
   function cyclePets() {
-    if (active === 3) active = 0;
-    else active++;
-    makePetActive(active);
-  
-    setTimeout(cyclePets, getRandomTime(1,5))
+    makePetActive(getRandom(0, numPets - 1));
+    setTimeout(cyclePets, getRandom(3,6) * 1000)
   }
 
-  setTimeout(cyclePets, getRandomTime(1, 5));
+  fetch('/photos.txt')
+    .then(response => response.text())
+    .then(pets => {
+      const petsList = pets.split('\n');
+      petsList.pop();
+      numPets = petsList.length;  
+      const container = document.getElementsByClassName('photos-container')[0];
+      petsList.forEach((pet, index) => {
+        const img = document.createElement('img');
+        img.src = `/photos/${pet}`;
+        img.id = `pet-${index}`;
+        if (index === 0) img.className = 'active';
+        container.appendChild(img);
+      });
+      
+      setTimeout(cyclePets, getRandom(3,6) * 1000);
+    });
 })();
